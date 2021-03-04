8d002a65d34335c14440e0f3be1f90435ca7a96e
Cameron Aragon <caragon4695@gmail.com>
2021-03-04 16:48:53 -0700

    Display header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,19 @@
 import react from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
         <header>
+            <div className='user-profile'>
+                <img className='user-image' src='https://e7.pngegg.com/pngimages/448/589/png-clipart-bartender-pouring-alcoholic-beverage-in-martini-glass-cocktail-beer-bartender-illustration-cartoon-bartender-miscellaneous-cartoon-character.png'></img>
+                <div className='user-info'>
+                    <p>Bartender Name</p>
+                    <p>Bartender Level</p>
+                    <p>Experience Bar</p>
+                </div>
+            </div>
             <h1> MIXitUP</h1>
+            <h3>Level Up Your Bartending Skills!</h3>
         </header>
     )
 }
