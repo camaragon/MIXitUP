7b721bf9ff46bff79ea49a3b73f04b724d29d642
Cameron Aragon <caragon4695@gmail.com>
2021-03-04 21:30:43 -0700

    Style the Cocktail card

--- src/Components/Cocktail/Cocktail.js
+++ src/Components/Cocktail/Cocktail.js
@@ -2,8 +2,8 @@ import './Cocktail.css';
 
 const Cocktail = () => {
     return (
-        <div>
-            <img></img>
+        <div className='cocktail-card'>
+            <img className='cocktail-image' src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2020%2F01%2Fnon-alcoholic-drinks-FT-BLOG0120-2.jpg'></img>
             <h3>Drink Name</h3>
             <p>Drink Type</p>
         </div>
