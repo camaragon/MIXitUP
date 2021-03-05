90246fe0050b2d4fbdc0314b352d17890ce84274
Cameron Aragon <caragon4695@gmail.com>
2021-03-04 20:58:14 -0700

    Build Cocktail component and import

---
+++ src/Components/Cocktail/Cocktail.js
@@ -0,0 +1,13 @@
+import './Cocktail.css';
+
+const Cocktail = () => {
+    return (
+        <div>
+            <img></img>
+            <h3>Drink Name</h3>
+            <p>Drink Type</p>
+        </div>
+    )
+}
+
+export default Cocktail;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,11 @@
 import './Main.css';
+import Cocktail from '../Cocktail/Cocktail';
 
 const Main = () => {
     return (
         <main className='main'>
             <h3 className='bartend-tips'>Bartender Tip</h3>
+            <Cocktail />
             <div className='button-container'>
                 <button>Reccomended Tools</button>
                 <button>Generate a Cocktail</button>
