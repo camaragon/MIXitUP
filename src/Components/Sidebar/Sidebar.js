1abd90f0c6861c47a28472ecf5e778a11155a1ab
Cameron Aragon <caragon4695@gmail.com>
2021-03-05 23:43:17 -0700

    Add styling for the made drinks cards

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,6 +4,18 @@ const Sidebar = () => {
     return (
         <section>
             <h3 className='made-title'>Made Drinks</h3>
+            <div className='made-drink-card'>
+                <h3>Drink</h3>
+                <img className='made-image' src='https://www.boliviabella.com/images/bolivia_facts_bolivian_food_recipes_purple_corn_api.jpg'></img>
+            </div>
+            <div className='made-drink-card'>
+                <h3>Drink</h3>
+                <img className='made-image' src='https://www.boliviabella.com/images/bolivia_facts_bolivian_food_recipes_purple_corn_api.jpg'></img>
+            </div>
+            <div className='made-drink-card'>
+                <h3>Drink</h3>
+                <img className='made-image' src='https://www.boliviabella.com/images/bolivia_facts_bolivian_food_recipes_purple_corn_api.jpg'></img>
+            </div>
         </section>
     )
 }
