58bb38daf8f1316a93a976ab8ce8b11277404ac7
Cameron Aragon <caragon4695@gmail.com>
2021-03-05 22:26:36 -0700

    Add styling for recipe info display

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -2,14 +2,14 @@ import './Recipe.css';
 
 const Recipe = ({name, image, category, glass, instructions, ingredients}) => {
     return (
-        <section>
+        <div className='recipe'>
+            <img className='recipe-image' src={image}></img>
             <div className='recipe-info'>
-                <p>Hello</p>
                 <h2>{name}</h2>
                 <p>{category}</p>
                 <p> Glass Type: {glass}</p>
                 {Array.isArray(ingredients) ? 
-                    <ul>Ingredients:{ingredients.map((ingredient, index) => {
+                    <ul>Ingredients: {ingredients.map((ingredient, index) => {
                         if (!ingredient.includes('null')) {
                             return <li key={index}>{ingredient}</li>
                         }
@@ -17,9 +17,9 @@ const Recipe = ({name, image, category, glass, instructions, ingredients}) => {
                     </ul> : 
                     <p>Ingredients: {ingredients}</p>}
                     <p>{instructions}</p>
+                    <button>Go Back</button>
             </div>
-            <img className='recipe-image' src={image}></img>
-        </section>
+        </div>
     )
 }
 
