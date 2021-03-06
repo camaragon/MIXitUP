82ba7b1c552c7e6a9447bce8e22a997a4c0613c6
Cameron Aragon <caragon4695@gmail.com>
2021-03-05 21:26:42 -0700

    Display recipe info

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -1,8 +1,25 @@
 import './Recipe.css';
 
-const Recipe = () => {
+const Recipe = ({name, image, category, glass, instructions, ingredients}) => {
     return (
-        <h2>Cocktail Recipe Info</h2>
+        <section>
+            <div className='recipe-info'>
+                <p>Hello</p>
+                <h2>{name}</h2>
+                <p>{category}</p>
+                <p> Glass Type: {glass}</p>
+                {Array.isArray(ingredients) ? 
+                    <ul>Ingredients:{ingredients.map((ingredient, index) => {
+                        if (!ingredient.includes('null')) {
+                            return <li key={index}>{ingredient}</li>
+                        }
+                    })}
+                    </ul> : 
+                    <p>Ingredients: {ingredients}</p>}
+                    <p>{instructions}</p>
+            </div>
+            <img className='recipe-image' src={image}></img>
+        </section>
     )
 }
 
