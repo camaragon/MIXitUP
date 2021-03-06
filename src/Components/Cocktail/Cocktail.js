983a7764a834cf547f3d11d3d6378cfde2731296
Cameron Aragon <caragon4695@gmail.com>
2021-03-05 20:13:57 -0700

    Move conditional rendering to Main component

--- src/Components/Cocktail/Cocktail.js
+++ src/Components/Cocktail/Cocktail.js
@@ -1,26 +1,22 @@
 import './Cocktail.css';
+import {Link} from 'react-router-dom'
 
-const Cocktail = ({cocktail}) => {
+const Cocktail = ({cocktail, name, category, image, id}) => {
     console.log(cocktail);
     return (
-       (cocktail) ?
         <div className='cocktail-card'>
-            <img className='cocktail-image' src={cocktail[0].strDrinkThumb}></img>
+            <img className='cocktail-image' src={image}></img>
             <div className='horizontal'>
                 <button>Make Drink</button>
                 <div className='stacked'>
-                    <h3>{cocktail[0].strDrink}</h3>
-                    <p>{cocktail[0].strCategory}</p>
+                    <h3>{name}</h3>
+                    <p>{category}</p>
                 </div>
                 <Link to={`/${id}`} >
                     <button>Drink Recipe</button>
                 </Link>
             </div>
-        </div> : 
-        <div className='no-card'>
-            <p>Empty</p>
         </div> 
-    )
 }
 
 export default Cocktail;
