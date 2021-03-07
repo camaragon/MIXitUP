7669102f9309414716af877fc84251babed0bf56
Cameron Aragon <caragon4695@gmail.com>
2021-03-07 16:45:02 -0700

    Setup URL in homepage test

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,3 +1,24 @@
 describe('Homepage', () => {
-    const baseUrl = 'http://localhost:3000'
+    const baseUrl = 'http://localhost:3000';
+
+    it('Should be able to visit the base URL and see the homepage', () => {
+        cy
+        .visit(baseUrl)
+        .location().should((loc) => {
+            expect(loc.host).to.eq('localhost:3000')
+            expect(loc.origin).to.eq(baseUrl)
+        })
+    });
+
+    beforeEach(() => {
+        cy
+        .visit(baseUrl);
+    });
+
+    it('Should display a heading with a name of the app', () => {
+        cy
+        .get('header').within(() => {
+            cy.get('h1').contains('MIX it UP')
+        })
+    })
 })
