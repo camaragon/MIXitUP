1297fdbc38c2493e7817bd1cc19e7ab647b3ee35
Cameron Aragon <caragon4695@gmail.com>
2021-03-07 17:29:33 -0700

    Finish testing intitial header display

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -21,4 +21,24 @@ describe('Homepage', () => {
             cy.get('h1').contains('MIX it UP')
         })
     })
+
+    it('Should display user\'s profile with an image, name, level, and progress bar', () => {
+        cy
+        .get('header').within(() => {
+            cy.get('.user-profile').within(() => {
+                cy
+                .get('p').contains('Leta Keane')
+                .get('p').contains('Level')
+                .get('progress').should('be.visible')
+                .get('.user-image').should('be.visible')
+            })
+        })
+    })
+
+    it('Should display instructions to user in the header', () => {
+        cy
+        .get('header').within(() => {
+            cy.get('.header-quote').contains('Level Up Your Bartending Skills!For every 3 drinks made you level up!Only select Make Drink if you really made the drink!')
+        })
+    })
 })
