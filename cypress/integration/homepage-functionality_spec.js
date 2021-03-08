78aabb187c96b03d015c9a680ddbeea991f6ca0f
Cameron Aragon <caragon4695@gmail.com>
2021-03-07 18:13:03 -0700

    Add new cypress testing file for functionality

---
+++ cypress/integration/homepage-functionality_spec.js
@@ -0,0 +1,17 @@
+describe('Homepage', () => {
+    const baseUrl = 'http://localhost:3000';
+
+    it('Should be able to visit the base URL and see the homepage', () => {
+        cy
+        .visit(baseUrl)
+        .location().should((loc) => {
+            expect(loc.host).to.eq('localhost:3000')
+            expect(loc.origin).to.eq(baseUrl)
+        })
+    });
+
+    beforeEach(() => {
+        cy
+        .visit(baseUrl);
+    });
+})
